833e0d5e5215199ce8d82ad26586e024e8324592
buciorgams123 <75723256+buciorgams123@users.noreply.github.com>
2021-12-17 15:53:42 +0100

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,18 +1,18 @@
 module.exports = {
-	kanalkomend: "809881262512013312", //id kanalu z komendami
-	kanalwspolprac: "827148325706792990", // id kanalu z partnerstrwami
-	dbhost: "1.1.1.1.1", // ip do bazy
+	kanalkomend: "918194381821993070", //id kanalu z komendami
+	kanalwspolprac: "919598969900912711", // id kanalu z partnerstrwami
+	dbhost: "137.74.6.118", // ip do bazy
 	dbport: "3306", //port bazy
-	dbuser: "db_12345", //nazwa użyt bazy
-	dbpass: "123456", //haslo bazy
-	dbname: "db_12345", //nazwa bazy
-	activitytype: "custom", // custom czyli costomowa albo online czyli w activity piszesz np. Online: i będzie wyświetlac Online: 0/50
+	dbuser: "db_68006", //nazwa użyt bazy
+	dbpass: "bittorent55", //haslo bazy
+	dbname: "db_68005", //nazwa bazy
+	activitytype: "online", // custom czyli costomowa albo online czyli w activity piszesz np. Online: i będzie wyświetlac Online: 0/50
 	activity: "aktualnie:", // tutaj jak dajesz custom piszesz byle co a jak online to po wiadomosci będzie 0/50
-	statushost: "51.83.182.220", //host mta
-	statusport: "20108", // port mta
-	typnagrody: "kasa", // auto albo kasa
-	modelauta: 502, // model auta z mtawiki
+	statushost: "54.38.202.159", //host mta
+	statusport: "21292", // port mta
+	typnagrody: "auto", // auto albo kasa
+	modelauta: 560, // model auta z mtawiki
 	nagrodailosc: "1", //ilosc kasy jezeli hajsiwko
-    token: "NzczODE1MjY0NzYzNTc2MzUy.X6Otnw.JBi0XT7tJIiwz8sQk6eUNweCni0", // token bota
+    token: "OTAyNDg2MzQ1NzQ5MDAwMjQy.YXfH6A.5NCma8-_rj-v-ifwtGaISh_xbCg", // token bota
     prefix: "!",
 }
