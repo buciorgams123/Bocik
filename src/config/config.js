d7b5329bdf1e1583b9cb4afe0698b2ae31cced48
buciorgams123 <75723256+buciorgams123@users.noreply.github.com>
2021-12-17 15:49:01 +0100

    Add files via upload

---
+++ src/config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	kanalkomend: "809881262512013312", //id kanalu z komendami
+	kanalwspolprac: "827148325706792990", // id kanalu z partnerstrwami
+	dbhost: "1.1.1.1.1", // ip do bazy
+	dbport: "3306", //port bazy
+	dbuser: "db_12345", //nazwa użyt bazy
+	dbpass: "123456", //haslo bazy
+	dbname: "db_12345", //nazwa bazy
+	activitytype: "custom", // custom czyli costomowa albo online czyli w activity piszesz np. Online: i będzie wyświetlac Online: 0/50
+	activity: "aktualnie:", // tutaj jak dajesz custom piszesz byle co a jak online to po wiadomosci będzie 0/50
+	statushost: "51.83.182.220", //host mta
+	statusport: "20108", // port mta
+	typnagrody: "kasa", // auto albo kasa
+	modelauta: 502, // model auta z mtawiki
+	nagrodailosc: "1", //ilosc kasy jezeli hajsiwko
+    token: "NzczODE1MjY0NzYzNTc2MzUy.X6Otnw.JBi0XT7tJIiwz8sQk6eUNweCni0", // token bota
+    prefix: "!",
+}
